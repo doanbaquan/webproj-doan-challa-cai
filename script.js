708a5ae26c3c8307a103515c9340342b2818f8f5
Eric <94411403+ericcai2005@users.noreply.github.com>
2021-12-19 19:00:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+
+document.writeln("<div id=\"contain\">");
+document.writeln("        <img class=\"centerimg\" src=\"../images/download.jpg\" alt=\"Formula1Car\" width=\"400\" height=auto >");
+document.writeln("            <div class=\"text\">This is an image of a customized Lamborghini used for street racing. These cars are typically very flashy and bright, as to attract attention and stand out.</div>");
+document.writeln("    </div>");
